72a4385108e87b041e36ec74715b537fbb3da650
Cristhian Carrillo <carrillocristhian2408@gmail.com>
2023-02-04 21:20:35 -0500

    first one

---
+++ src/pages/home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="bg-green-200 h-screen pt-4">
+      <p className="text-center font-extrabold text-4xl">Orders Manager</p>
+    </div>
+  );
+}
+
+export default Home;
